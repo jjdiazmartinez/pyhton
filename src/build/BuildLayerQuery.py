8a231b40c65a4742a7c2aa5bb60ce3fb85ac1cc3
jjdiaz <jjdiaz@softcaribbean.com>
2023-06-07 19:38:59 -0500

    Se agrega el manejo de insert y update

--- src/build/BuildLayerQuery.py
+++ src/build/BuildLayerQuery.py
@@ -1,20 +1,31 @@
 
 import src.build.dml.BuildQuery as bdqu
+import src.build.dml.BuildDml as budml
 import src.io.ManagerFile as managerfile
 import src.java.util.Package as package
 class BuildLayerQuery:
     def buildComponent(self,structure):
         token=[]
         bdquery = bdqu.BuildQuery()
+        buildDml=budml.BuildDml()
         pack = package.Package()
         managerFile = managerfile.ManagerFile()
         for item in structure:
             textFile=bdquery.buildSelectForId(structure[item])
             textFileForAll = bdquery.buildSelectForAll(structure[item])
+            textInsert = buildDml.buildInsert(structure[item])
+            textUpdate = buildDml.buildUpdate(structure[item])
             token.append('"select_'+structure[item]["name"]+'_byid":')
             token.append('"'+textFile+'",')
+
             token.append('"select_' + structure[item]["name"] + '_forall":')
             token.append('"' + textFileForAll + '",')
+
+            token.append('"insert_' + structure[item]["name"] + '":')
+            token.append('"' + textInsert + '",')
+
+            token.append('"update_' + structure[item]["name"] + '":')
+            token.append('"' + textUpdate + '",')
 
         path=pack.create("D:/temp/","sql")
         managerFile.write("D:/temp/"+path+"/sql.json","{"+"".join(token)[0:-1]+"}")
---
+++ src/build/dml/BuildDml.py
@@ -0,0 +1,41 @@
+import src.build.shared.SharedTemplate as template
+import src.common.UtilDictionary as utildic
+class BuildDml:
+    def buildInsert(self, structureDto):
+        templ = template.SharedTemplate()
+        self.structuredto = structureDto
+        self.structuregeneral = templ.getStructure()
+        values = self.doInsert()
+        return "INSERT INTO "+self.structuredto["table"]+" "+values
+
+    def buildUpdate(self, structureDto):
+        templ = template.SharedTemplate()
+        self.structuredto = structureDto
+        self.structuregeneral = templ.getStructure()
+        values = self.doUpdate()
+        where = self.doWhere()
+        return "UPDATE  " + self.structuredto["table"] + " SET " + values+ " "+where
+
+    def doInsert(self):
+        tokenField=[]
+        for item in self.structuredto["attributes"]:
+            tokenField.append(item["name"]+",")
+        token=[]
+        for item in self.structuredto["attributes"]:
+            token.append("?,")
+
+        return "("+"".join(tokenField)[0:-1]+") values ("+ "".join(token)[0:-1]+") "
+
+    def doUpdate(self):
+        tokenField=[]
+        listNoPk = list(utildic.filter_dict_by_attribute(self.structuredto["attributes"], "pk", "n"))
+        for item in listNoPk:
+            tokenField.append(item["name"]+"=?,")
+        return "".join(tokenField)[0:-1]
+
+    def doWhere(self):
+
+        token = []
+        listPk = list(utildic.filter_dict_by_attribute(self.structuredto["attributes"], "pk", "y"))
+        token.append(listPk[0]["name"] + " = ?")
+        return " WHERE "+"".join(token)
